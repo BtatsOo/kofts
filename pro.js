493a8a1a49c67ab4de579a87b40d49cbe57da9d5
BtatsOo <73794774+BtatsOo@users.noreply.github.com>
2023-03-02 21:51:43 +0200

    Update pro.js

--- pro.js
+++ pro.js
@@ -3,10 +3,10 @@ y = document.getElementById("el7mar");
 function myfunction() {
   val = document.querySelector("#input");
   message = document.querySelector("#test");
-  message.innerHTML = "welcome " + val.value;
+  message.innerHTML = "welcome " + val.value + " in 7meer company! ";
   if (val.value === "واعدو") {
     x.play();
-  } else if ((val.value = "Waleed-Alaa")) {
+  } else if (val.value) {
     y.play();
     document.getElementById("img").style.display = "block";
   }
